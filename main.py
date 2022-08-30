503b075fe6ba893329aad14a6ab756992c494e50
Eric Jing <jinge9475@gmail.com>
2022-08-30 14:39:15 -0400

    Basic Tracking

---
+++ main.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+import mediapipe as mp
+import time
+
+cap = cv.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    cv.imshow("Image", img)
+    cv.waitKey(1)
